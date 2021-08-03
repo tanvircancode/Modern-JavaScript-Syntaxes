123678d02ada1babfc25154393cf8573e55f856b
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-08-03 23:32:03 +0600

    UPDATED

---
+++ ternary_operator.js
@@ -0,0 +1,12 @@
+var age = 9;
+
+var type = (age >= 18) ? "Adult" : (age<10) ? "Child" : "Young" ;
+
+
+// if (age < 18) {
+//     type = "Child";
+// }
+// else {
+//     type = "adult";
+// }
+console.log(type);
